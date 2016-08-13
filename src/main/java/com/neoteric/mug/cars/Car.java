2fd5864d793b656fa4db36decc736e1b97abadae
trichert <trichert@neoteric.eu>
2016-08-13 12:42:51 +0200

    added index

--- src/main/java/com/neoteric/mug/cars/Car.java
+++ src/main/java/com/neoteric/mug/cars/Car.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 import lombok.Builder;
 import lombok.Value;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.ZonedDateTime;
@@ -12,6 +14,7 @@ import java.time.ZonedDateTime;
 @Builder(toBuilder = true)
 @JsonDeserialize(builder = Car.CarBuilder.class)
 @Document
+@CompoundIndexes({@CompoundIndex(name = "brand_model", def = "{ 'brand': 1, 'model': 1}", unique = true)})
 public class Car {
 
     String id;
